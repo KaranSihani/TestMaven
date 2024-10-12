67e1d65cab8618d762cfdc479e62a3946f2f5221
Karan Sihani <karan.sihani@nagarro.com>
2024-10-12 15:24:09 +0530

    First Commit

---
+++ src/test/java/Testpackage/testclass.java
@@ -0,0 +1,36 @@
+package Testpackage;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+public class testclass {
+
+	int additionValue = 8;
+	int subtractValue = 2;
+	int multiplicationValue = 15;
+	
+@Test
+public void addition() {
+	int a = 5;
+	int b=3;
+	int finaladd = a+b;
+	assertEquals(finaladd, additionValue);	
+}
+@Test
+public void subtraction() {
+	int a = 5;
+	int b=3;
+	int finalsub = a-b;
+	assertEquals(finalsub, subtractValue);	
+}
+@Test
+public void multiply() {
+	int a = 5;
+	int b=3;
+	int finalmult = a*b;
+	assertEquals(finalmult, multiplicationValue);	
+}
+
+
+}
